cac0a2e366c5ac270f8aa21daf483099fae54321
Gage <35212618+SokimMan@users.noreply.github.com>
2020-07-11 10:15:39 -0600

    database setup and display

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const cool = require('cool-ascii-faces');
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+const connectionString = process.env.DATABASE_URL || "postgres://ywtcjxlcityric:c361ad6a0fa2a243560478dbcef8c5bb5aad5acefa50e67cf9c43a993a74c270@ec2-54-234-28-165.compute-1.amazonaws.com:5432/d5s7bcbtu1lj5c?ssl=true";
 
 
 const { Pool } = require('pg');
+/*
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true//{
-    //rejectUnauthorized: false
-  //}
-});
+  connectionString: connectionString,
+  ssl: {
+    rejectUnauthorized: false
+  	}
+}); */
+const pool = new Pool({connectionString: connectionString});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))

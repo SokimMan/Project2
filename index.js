43125b4a1b66d36722a45684803175b878f8432e
Gage <35212618+SokimMan@users.noreply.github.com>
2020-07-11 08:19:29 -0600

    db setup

--- index.js
+++ index.js
@@ -3,22 +3,21 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-/*
+
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: true//{
+    //rejectUnauthorized: false
+  //}
 });
-*/
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  /*.get('/db', async (req, res) => {
+  .get('/db', async (req, res) => {
     try {
       const client = await pool.connect();
       const result = await client.query('SELECT * FROM test_table');
@@ -30,6 +29,5 @@ express()
       res.send("Error " + err);
     }
   })
-  */
   .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))

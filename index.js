d79a6fa741cae6f0906156497b90db7d53e4195b
Gage <35212618+SokimMan@users.noreply.github.com>
2020-07-15 17:21:40 -0600

    dropped ajax that was breaking project

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+
   .get('/', (req, res) => res.render('pages/index'))
   
   // Dev Path to see DB post
@@ -68,9 +69,10 @@ express()
 
   })
   */
-  
+  .get('/searchUser', getPerson)
 
   .get('/newUser', createNewUser)
+ 
   .get('/cool', (req, res) => res.send(cool()))
 
 
@@ -87,7 +89,7 @@ express()
   		{
       		const client = await pool.connect();
       		const result = await client.query(command);
-      		return result;
+      		//return result;
       		client.release();
     	}	 	
     	catch (err) 
@@ -96,7 +98,7 @@ express()
       		res.send("Error " + err);
     	}
 
-    	return result;
+    	//return result;
 
   	}
 
@@ -136,3 +138,63 @@ express()
 	// Render the response, using the EJS page "result.ejs" in the pages directory
 	response.render('pages/result', params);
     }
+
+// This function handles requests to the /getPerson endpoint
+// it expects to have an id on the query string, such as: http://localhost:5000/getPerson?id=1
+function getPerson(request, response) {
+	// First get the person's id
+	const id = request.query.id;
+
+	// TODO: We should really check here for a valid id before continuing on...
+
+	// use a helper function to query the DB, and provide a callback for when it's done
+	getPersonFromDb(id, function(error, result) {
+		// This is the callback function that will be called when the DB is done.
+		// The job here is just to send it back.
+
+		// Make sure we got a row with the person, then prepare JSON to send back
+		if (error || result == null || result.length != 1) {
+			response.status(500).json({success: false, data: error});
+		} else {
+			const person = result[0];
+			response.status(200).json(person);
+		}
+	});
+}
+
+// This function gets a person from the DB.
+// By separating this out from the handler above, we can keep our model
+// logic (this function) separate from our controller logic (the getPerson function)
+function getPersonFromDb(id, callback) {
+	console.log("Getting person from DB with id: " + id);
+
+	// Set up the SQL that we will use for our query. Note that we can make
+	// use of parameter placeholders just like with PHP's PDO.
+	const sql = "SELECT * FROM sf WHERE externalID ='" + id + "';";
+
+	// We now set up an array of all the parameters we will pass to fill the
+	// placeholder spots we left in the query.
+	const params = [id];
+
+	// This runs the query, and then calls the provided anonymous callback function
+	// with the results.
+	pool.query(sql, params, function(err, result) {
+		// If an error occurred...
+		if (err) {
+			console.log("Error in query: ")
+			console.log(err);
+			callback(err, null);
+		}
+
+		// Log this to the console for debugging purposes.
+		console.log("Found result: " + JSON.stringify(result.rows));
+
+
+		// When someone else called this function, they supplied the function
+		// they wanted called when we were all done. Call that function now
+		// and pass it the results.
+
+		// (The first parameter is the error variable, so we will pass null.)
+		callback(null, result.rows);
+	});
+}

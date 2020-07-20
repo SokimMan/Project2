9d670309d72c8590adb256a24ecf2c97b65b1175
Gage <35212618+SokimMan@users.noreply.github.com>
2020-07-19 23:00:09 -0600

    t

--- index.js
+++ index.js
@@ -156,13 +156,15 @@ function getPerson(request, response)
 	// TODO: We should really check here for a valid id before continuing on...
 
 	// use a helper function to query the DB, and provide a callback for when it's done
+  /*
 	getPersonFromDb(id, function(error, result) 
   {
 		// This is the callback function that will be called when the DB is done.
 		// The job here is just to send it back.
     //const params = {externalID: externalID, firstName: firstName, lastName: lastName, city: city}
 		// Make sure we got a row with the person, then prepare JSON to send back
-		if (error || result == null) { // result.length != 1) {
+		/*
+    if (error || result == null) { // result.length != 1) {
 			response.status(500).json({success: false, data: error});
 		} else {
 			const person = result.rows;//[0];
@@ -171,7 +173,34 @@ function getPerson(request, response)
       console.log("logger: " + JSON.stringify(person));
       response.render('pages/search', person);
 		}
+    
 	});
+  */
+
+  console.log("Getting person from DB with id: " + id);
+
+  // Set up the SQL that we will use for our query. Note that we can make
+  // use of parameter placeholders just like with PHP's PDO.
+  const sql = "SELECT * FROM sf WHERE external_ID = $1::text" //'" + id + "';";
+
+  // We now set up an array of all the parameters we will pass to fill the
+  // placeholder spots we left in the query.
+  const params = [id];
+
+  // This runs the query, and then calls the provided anonymous callback function
+  // with the results.
+  pool.query(sql, params, function(err, result) {
+    // If an error occurred...
+    if (err) {
+      console.log("Error in query: ")
+      console.log(err);
+      callback(err, null);
+    }
+
+    // Log this to the console for debugging purposes.
+    console.log("Found result: " + JSON.stringify(result.rows));
+    response.render('pages/result', result.rows);
+
 }
 
 // This function gets a person from the DB.
@@ -207,6 +236,6 @@ function getPersonFromDb(id, callback) {
 		// and pass it the results.
 
 		// (The first parameter is the error variable, so we will pass null.)
-		callback(null, result.rows);
+		//callback(null, result.rows);
 	});
 }
